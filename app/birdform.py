93eccac559c5a7a2d50da042be40639ed69a3a18
Faro145 <60946696+Faro145@users.noreply.github.com>
2020-11-11 15:22:01 +0000

    Update birdform.py

--- app/birdform.py
+++ app/birdform.py
@@ -1,7 +1,8 @@
 from flask_wtf import FlaskForm
-from wtforms import IntegerField, DateTimeField, StringField, SubmitField
+from wtforms import StringField, SubmitField
 from app.models import Birds
 
 class birdForm(FlaskForm):
   scientific_name = StringField('Scientific Name')
   common_name = StringField('Common Name')
+  submit = SubmitField('Add Bird')
