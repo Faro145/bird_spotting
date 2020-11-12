11695fbf5baacdff106fea66f3586ea8f0622fe4
Faro145 <rossfarq@gmail.com>
2020-11-12 16:42:26 +0000

    improve form and validation

--- app/form.py
+++ app/form.py
@@ -4,20 +4,20 @@ from wtforms.validators import DataRequired, ValidationError
 from app.models import Locations, Birds, Sightings
 
 class CountryCheck:
-    def __init__(self, ukcountry, message=none):
+    def __init__(self, ukcountry, message):
         self.ukcountry = ukcountry
         if not message:
             message = "Not a UK country"
         self.message = message
 
     def __call__(self, form, field):
-        if field.data != self.ukcountry:
+        if field.data != self.ukcountry[0] and field.data != self.ukcountry[1] and field != self.ukcountry[2] and field != self.ukcountry[3]:
             raise ValidationError(self.message)
 
 class locationForm(FlaskForm):
   place_name = StringField('Place Name', validators = [DataRequired(),])
   county = StringField('County', validators = [DataRequired(),])
-  country = StringField('Country', validators = [DataRequired(), CountryCheck(message="Not a UK Country", ukcountry['Scotland', 'England', 'Northern Ireland', 'Wales']), ])
+  country = StringField('Country', validators = [DataRequired(), CountryCheck(ukcountry = ['Scotland', 'England', 'Northern Ireland', 'Wales'], message="Not a UK Country"), ])
   submit = SubmitField('Add Location')
 
 class birdForm(FlaskForm):
