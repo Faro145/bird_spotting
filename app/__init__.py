77095bd086ecccaf41403c268141c526d89959df
Faro145 <ross@gmail.com>
2020-11-13 10:08:25 +0000

    getenv and checking database

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from os import getenv
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///data.db"
-app.config['SECRET_KEY'] = "YOUR_SECRET_KEY"
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DB_URI')
+app.config['SECRET_KEY'] = os.getenv('KEY')
 
 db = SQLAlchemy(app)
 
